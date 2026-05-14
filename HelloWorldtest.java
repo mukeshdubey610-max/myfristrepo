1ea3bcb27ddbb31dca13fcb99c054e87a09be51e
mukeshdubey610-max <mukeshdubey610@gmail.com>
2026-05-14 23:50:29 +0530

    Update HelloWorldtest.java

--- HelloWorldtest.java
+++ HelloWorldtest.java
@@ -1,5 +1,5 @@
 public class HelloWorldtest {
     public static void main(String[] args) {
-        System.out.println("Hello, Mukesh, This is 2nd testing!");
+        System.out.println("Hello, Mukesh, This is 2nd testing, now testing automation!");
     }
 }
